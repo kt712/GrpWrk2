462185803ea55d2477ca1ae87d8de7e6ce48007a
Ismail Kabir <ismailkbalarabe@gmail.com>
2023-01-22 11:45:35 +0400

    added lessons json data

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var express = require("express");
 var app = express();
+
+let lessons = [
+    {'topic':'chemistry','location':'Sharjah','price':'100'},
+    {'topic':'chemistry','location':'Abu Dhabi','price':'90'},
+    {'topic':'chemistry','location':'Dubai','price':'80'},
+    {'topic':'chemistry','location':'Dubai','price':'150'},
+];
